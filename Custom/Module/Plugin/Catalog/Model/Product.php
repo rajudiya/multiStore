b5c68d82e5ccce400f2ede7c6b3b70af7901c9b4
RohanAjudiya <75730908+rajudiya@users.noreply.github.com>
2023-08-08 18:29:14 +0530

    Add files via upload

--- Custom/Module/Plugin/Catalog/Model/Product.php
+++ Custom/Module/Plugin/Catalog/Model/Product.php
@@ -2,11 +2,54 @@
 namespace Custom\Module\Plugin\Catalog\Model ;
 class Product
 {
-    public function afterGetProductCollection(\Magento\Catalog\Model\Layer $subject, $result) {
-        //$result->addAttributeToSort('brand','ASC'); //DESC for descending.
-        $result->addAttributeToSort('product_name_english');
-        $result->setName('Rohan'); //For dropdown type attribute you can use this.
-        return $result;
+    public function afterGetName(\Magento\Catalog\Model\Product $subject, $result)
+    {
+        $storeManager = \Magento\Framework\App\ObjectManager::getInstance()->get('\Magento\Store\Model\StoreManagerInterface');
+        $sid = $storeManager->getStore()->getStoreId();
+        $wid = $storeManager->getStore()->getWebsiteId();
+        if ($sid == 2 ||$sid == 4 || $sid == 7 ) {
+            if($subject->hasData('product_name_english'))
+            {                
+            return $subject->getData('product_name_english');             
+            }
+        }elseif ($sid == 3 ||$sid == 5 || $sid == 6) {
+            if($subject->hasData('product_name_france')) {
+                return $subject->getData('product_name_france');
+            }
+        }else {
+            return $subject->getData('name');
+        }
+    }
+
+    //price
+    public function afterGetPrice(\Magento\Catalog\Model\Product $subject, $result)
+    {
+        $storeManager = \Magento\Framework\App\ObjectManager::getInstance()->get('\Magento\Store\Model\StoreManagerInterface');
+        $sid = $storeManager->getStore()->getStoreId();
+        $wid = $storeManager->getStore()->getWebsiteId();
+        if ($wid == 2) {
+            if($subject->hasData('price_web_1') >= 1)
+            {                
+                $price = $subject->getData('price');
+                $price = $subject->getData('price_web_1');
+                return  $price;           
+            }
+        }elseif ($wid == 2) {
+            if($subject->hasData('price_web_2')) {
+                $price = $subject->getData('price');
+                $price = $subject->getData('price_web_2');
+                return  $price;
+            }
+        }elseif ($wid == 3) {
+            if($subject->hasData('price_web_3')) {
+                $price = $subject->getData('price');
+                $price = $subject->getData('price_web_3');
+                return  $price;
+            }
+        }
+        else {
+            return $subject->getData('price');
+        }
     }
 }
 
